45b0607f56d6f8d227bf26049ce6f7cb5bde4592
Krishna Joshi <krishnarjoshi8080@gmail.com>
2023-03-02 02:29:03 +0530

    commit

--- src/main/java/com/example/cruddemo/model/Users.java
+++ src/main/java/com/example/cruddemo/model/Users.java
@@ -3,6 +3,8 @@ package com.example.cruddemo.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mongodb.lang.NonNull;
 import lombok.Data;
 import lombok.Getter;
@@ -13,45 +15,62 @@ import lombok.Getter;
 public class Users {
 	@Id
 	@NonNull
+	@JsonProperty("id")
 	String id;
-	
+
+	@JsonCreator
+	public Users() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@JsonProperty("firstName")
 	private String firstName;
+	@JsonProperty("lastName")
 	private String lastName;
+	@JsonProperty("email")
 	private String email;
+	@JsonProperty("phoneNumber")
 	private String phoneNumber;
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-	
-	
-	
 
 }
